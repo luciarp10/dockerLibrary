b7f9328f2df328ec86fd93bbd86578a5f654ef14
nicolaemolnar <nicolae.molnar@edu.uah.es>
2022-09-06 13:24:26 +0200

    Implemented /books endpoint
    Co-authored-by: luciarp10 <luciarp10@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -3,6 +3,16 @@ const body_parser = require('body-parser');
 const favicon = require('serve-favicon');
 const path = require('path');
 const utils = require('./utils');
+const fs = require('fs');
+const { env } = require('process');
+const os  = require('os');
+const { stringify } = require('querystring');
+
+const WORKDIR = env.WORKDIR;
+const booksDir = WORKDIR + "/books/";
+
+const version = env.VERSION ? env.VERSION : "1.0";
+
 // fn to create express server
 const create = async () => {
 
@@ -15,20 +25,54 @@ const create = async () => {
     app.use(utils.appLogger);
 
     // root route - serve static file
-    app.get('/hello', (req, res) => {
-        return res.sendFile(path.join(__dirname, '../public/hello.html'));
+    app.get('/', (req, res) => {
+        res.json({nodeName: os.hostname()
+            , version: version});
+        res.end();  
     });
 
-    app.post('/api/register', (req, res) => {
-        console.log(req.body)
-        res.json({name: req.body.name, surname: req.body.surname})
-        res.end()
+    app.post("/books", (req,res) => {
+        var ms = Date.now();
+        var datems = new Date(ms);
+        var day = datems.getDate();
+        var month = datems.getMonth()+1;
+        var year = datems.getFullYear();
+        
+        var date = day +"/"+month+"/"+year
+        var stream = fs.createWriteStream(booksDir+req.body.title+".txt");
+
+        stream.once('open', (fd) => {
+            stream.write('{\n"Title": "' + req.body.title + '",\n');
+            stream.write('"Date":"' + date + '",\n');
+            stream.write('"Content": "' + req.body.content +'"\n}')
+            stream.end();
+        });
+
+        res.json({
+            title: req.body.title,
+            date: date
+        })
+        res.end();
     })
 
-    // root route - serve static file
-    app.get('/', (req, res) => {
-        return res.sendFile(path.join(__dirname, '../public/client.html'));
-    });
+    app.get("/books", (req, res) => {
+        var books = []
+        fs.readdir(booksDir, function(err, files){
+            if (err){
+                res.status = 404;
+            }
+            
+            console.log(files);
+            files.forEach(function(file){
+                
+                books.push({fileName:file});
+            })
+            res.json(books)
+            res.end();
+        })  
+    })
+
+
 
     // Catch errors
     app.use(utils.logErrors);
